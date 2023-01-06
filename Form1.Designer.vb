8efc431f5fb603ef77516cdb369c45ad2c3f7e87
jayso <jayso@DESKTOP-U0KM5NI>
2023-01-06 14:22:42 +0800

    Add project files.

---
+++ Form1.Designer.vb
@@ -0,0 +1,142 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.StatusStrip1 = New System.Windows.Forms.StatusStrip()
+        Me.lblStatus = New System.Windows.Forms.ToolStripStatusLabel()
+        Me.btnSignin = New System.Windows.Forms.Button()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.tbPassword = New System.Windows.Forms.TextBox()
+        Me.cbUsername = New System.Windows.Forms.ComboBox()
+        Me.StatusStrip1.SuspendLayout()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'StatusStrip1
+        '
+        Me.StatusStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.lblStatus})
+        Me.StatusStrip1.Location = New System.Drawing.Point(0, 236)
+        Me.StatusStrip1.Name = "StatusStrip1"
+        Me.StatusStrip1.Size = New System.Drawing.Size(317, 22)
+        Me.StatusStrip1.TabIndex = 0
+        Me.StatusStrip1.Text = "StatusStrip1"
+        '
+        'lblStatus
+        '
+        Me.lblStatus.Name = "lblStatus"
+        Me.lblStatus.Size = New System.Drawing.Size(0, 17)
+        '
+        'btnSignin
+        '
+        Me.btnSignin.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!)
+        Me.btnSignin.Location = New System.Drawing.Point(208, 188)
+        Me.btnSignin.Name = "btnSignin"
+        Me.btnSignin.Size = New System.Drawing.Size(97, 37)
+        Me.btnSignin.TabIndex = 10
+        Me.btnSignin.Text = "Sign in"
+        Me.btnSignin.UseVisualStyleBackColor = True
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Location = New System.Drawing.Point(12, 12)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(293, 104)
+        Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox1.TabIndex = 9
+        Me.PictureBox1.TabStop = False
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!)
+        Me.Label2.Location = New System.Drawing.Point(12, 156)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(82, 20)
+        Me.Label2.TabIndex = 7
+        Me.Label2.Text = "Password:"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!)
+        Me.Label1.Location = New System.Drawing.Point(47, 125)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(47, 20)
+        Me.Label1.TabIndex = 8
+        Me.Label1.Text = "User:"
+        '
+        'tbPassword
+        '
+        Me.tbPassword.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!)
+        Me.tbPassword.Location = New System.Drawing.Point(100, 156)
+        Me.tbPassword.Name = "tbPassword"
+        Me.tbPassword.PasswordChar = Global.Microsoft.VisualBasic.ChrW(42)
+        Me.tbPassword.Size = New System.Drawing.Size(205, 26)
+        Me.tbPassword.TabIndex = 6
+        '
+        'cbUsername
+        '
+        Me.cbUsername.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.SuggestAppend
+        Me.cbUsername.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.cbUsername.FormattingEnabled = True
+        Me.cbUsername.Location = New System.Drawing.Point(100, 122)
+        Me.cbUsername.Name = "cbUsername"
+        Me.cbUsername.Size = New System.Drawing.Size(205, 28)
+        Me.cbUsername.TabIndex = 5
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(317, 258)
+        Me.Controls.Add(Me.btnSignin)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.tbPassword)
+        Me.Controls.Add(Me.cbUsername)
+        Me.Controls.Add(Me.StatusStrip1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.MaximumSize = New System.Drawing.Size(333, 297)
+        Me.MinimumSize = New System.Drawing.Size(333, 297)
+        Me.Name = "Form1"
+        Me.Text = "TiTA v3.0"
+        Me.StatusStrip1.ResumeLayout(False)
+        Me.StatusStrip1.PerformLayout()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents StatusStrip1 As StatusStrip
+    Friend WithEvents lblStatus As ToolStripStatusLabel
+    Friend WithEvents btnSignin As Button
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label1 As Label
+    Friend WithEvents tbPassword As TextBox
+    Friend WithEvents cbUsername As ComboBox
+End Class
---
+++ Form1.vb
@@ -0,0 +1,97 @@
+﻿Imports RestSharp
+Imports Newtonsoft.Json
+Public Class Form1
+    Public titaVersion As String = "3.0"
+    Dim accounts
+    'Classes Declaration
+    Public Class ColumnValue
+        Public Property title As String
+        Public Property text As String
+    End Class
+    Public Class Item
+        Public Property name As String
+        Public Property column_values As ColumnValue()
+    End Class
+    Public Class Board
+        Public Property items As Item()
+    End Class
+    Public Class Data
+        Public Property boards As Board()
+    End Class
+    Public Class Root
+        Public Property data As Data
+        Public Property account_id As Integer
+    End Class
+
+    Public Async Function SendMondayRequest(ByVal myQuery As String) As Task(Of String)
+        Dim options = New RestClientOptions("https://api.monday.com/v2")
+        options.ThrowOnAnyError = True
+        options.MaxTimeout = -1
+        Dim client = New RestClient(options)
+        Dim request = New RestRequest()
+        request.Method = Method.Post
+        request.AddHeader("Authorization", "eyJhbGciOiJIUzI1NiJ9.eyJ0aWQiOjE1MjU2NzQ3OCwidWlkIjoxNTA5MzQwNywiaWFkIjoiMjAyMi0wMy0yNVQwMTo0Njo1My4wMDBaIiwicGVyIjoibWU6d3JpdGUiLCJhY3RpZCI6NjYxMjMxMCwicmduIjoidXNlMSJ9.F1TqwLS-QsuM8Ss3UcgskbNFUIer1dfwfoLyPMq1pbc")
+        request.AddQueryParameter("query", myQuery)
+        Dim response = New RestResponse
+        response = Await client.ExecuteAsync(request)
+        Return response.Content
+    End Function
+
+    Public Function checkAccountDetails(ByVal surname As String, ByVal password As String, ByVal accounts As Root) As Boolean
+        '0 - First Name
+        '1 - Monday ID
+        '2 - Password
+        '3 - Department
+        For Each x In accounts.data.boards(0).items
+            If x.name = surname Then
+                'account found.
+                If x.column_values(2).text = password Then
+                    'account verified
+                    'save all account details in a global variable.
+                    MessageBox.Show("Success!")
+                Else
+                    MessageBox.Show("Incorrect Password.")
+                End If
+            End If
+        Next
+
+    End Function
+
+
+
+    Public Sub DisableAllControls()
+        tbPassword.Enabled = False
+        cbUsername.Enabled = False
+        btnSignin.Enabled = False
+    End Sub
+    Public Sub EnableAllControls()
+        tbPassword.Enabled = True
+        cbUsername.Enabled = True
+        btnSignin.Enabled = True
+    End Sub
+    Private Async Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Me.Text = $"Lasermet TiTA v{titaVersion}"
+        Dim fetchAccountQuery As String =
+            "query{
+                boards(ids:3428362986){
+                    items{
+                        name
+                        column_values{
+                            title
+                            text
+                        }
+                    }
+                }
+            }"
+        lblStatus.Text = "Fetching Accounts..."
+        DisableAllControls()
+        Dim result As String = Await SendMondayRequest(fetchAccountQuery)
+        accounts = JsonConvert.DeserializeObject(Of Root)(result)
+        EnableAllControls()
+        lblStatus.Text = "Accounts Fetched."
+    End Sub
+
+    Private Sub btnSignin_Click(sender As Object, e As EventArgs) Handles btnSignin.Click
+        checkAccountDetails(cbUsername.Text, tbPassword.Text, accounts)
+    End Sub
+End Class
